a9dfa2382119f309d9add61b35331c5f9889382a
erboro97 <erboro97@gmail.com>
2021-02-19 13:52:23 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DCsoportDemo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DCsoportDemo.dir/main.c.obj"
+  "DCsoportDemo.exe"
+  "DCsoportDemo.exe.manifest"
+  "DCsoportDemo.lib"
+  "DCsoportDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DCsoportDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void kiir(int);
+int main() {
+    setbuf(stdout, NULL);
+    int x=0, y[100][100]={0};
+    x++;
+    if(x<9){
+        x++;
+    }
+    kiir(x);
+    return 0;
+}
+void kiir(int x){
+    int y;
+    y=2;
+    printf("%d",x);
+}
